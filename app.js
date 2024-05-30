77820fd6aeedce85f252f6d8a969cbfad901d124
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-05-30 15:20:58 +0300

    connected to the mongo atlas

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-const express = require('express')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const app = express()
+dotenv.config({ path: "./config.env" });
 
-app.get('/' , (req, res)=>{
-  res.json({message:'hello from the server side', app:'Natours'})
-})
+const app = express();
 
-const port = 3000
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
 
-app.listen(port,()=>{
-  console.log(`Server in running on ${port}` )
-})
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB connection successful!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
+app.get("/", (req, res) => {
+  res.json({ message: "hello from the server side", app: "Natours" });
+});
+
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log(`Server in running on ${port}`);
+});
