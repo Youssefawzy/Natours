2cac29873c4ff6f7b19169b01a61717ec5ecb01e
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-05 11:31:01 +0300

    refactor( Tour API):  Refactor the  tours' API Features in utils  folder

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,51 +1,24 @@
 const Tour = require("./../modules/tourModel");
-
+const APIFeatures = require("./../utils/apiFeatures");
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
+    console.log("here");
+    let features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    //Filltering
-    const excludedFields = ["page", "sort", "limit", "fields"];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    let queryStr = JSON.stringify(queryObj); //adding the doller sign for better filttering
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    //sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } 
-    // else {
-    //   query = query.sort("-createdAt");
-    // }
-
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    console.log(req.query);
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    const tours = await query;
+    const tours = await features.query;
     res.status(200).json({
       status: "success",
       length: tours.length,
       tours,
     });
-  } catch (err) {
+  } catch (error) {
     res.status(404).json({
       status: "fail",
-      message: err,
+      error: { message: error.message, stack: error.stack },
     });
   }
 };
--- modules/tourModel.js
+++ modules/tourModel.js
@@ -63,10 +63,10 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a cover image"],
     },
     images: [String],
-    createAt: {
+    createdAt: {
       type: Date,
       default: Date.now(),
-      select: false,
+      select: true,
     },
     startDated: [Date],
     // secretTour: {
