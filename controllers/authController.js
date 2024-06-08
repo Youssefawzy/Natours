058ab51f6c7e4b0044a2ad9d2e3fc4a808bf4a7a
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-08 15:42:44 +0300

    feat(authentication): create the login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,14 @@
 const User = require("./../modules/userModel");
 const catchAsync = require("./../utils/catchAsync");
+const appError = require("./../utils/appError");
 const jwt = require("jsonwebtoken");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
 exports.signUp = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -17,5 +25,24 @@ exports.signUp = catchAsync(async (req, res, next) => {
     status: "success",
     token,
     newUser,
+  });
+});
+
+exports.login = catchAsync(async (req, res,next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return next(new appError("Please provide email and password", 400));
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password)))
+    return next(new appError("Incorrect email or password", 401));
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    token,
   });
 });
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,4 +4,6 @@ const authController = require("./../controllers/authController");
 
 router.route("/signup").post(authController.signUp);
 
+router.route("/login").post(authController.login);
+
 module.exports = router;
