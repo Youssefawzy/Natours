3310a7909794236cd476587a8e270293b6233ac7
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-09 17:22:44 +0300

    feat(authorization): create the restricted to function

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,3 +83,15 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restricTo = (roles) => {
+
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new appError("You do not have permission to perform this action", 403)
+      );
+    }
+    next();
+  };
+};
