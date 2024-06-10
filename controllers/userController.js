cbcac2880390bd9f4ffe54f95a63ccd5d09d7710
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-10 16:28:22 +0300

    feat(User): the user now can update his information

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
+const AppError = require("../utils/appError");
 const User = require("./../modules/userModel");
 const catchAsync = require("./../utils/catchAsync");
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -26,5 +35,28 @@ exports.deleteUser = catchAsync(async (req, res) => {
   res.status(204).json({
     status: "success",
     data: null,
+  });
+});
+
+exports.updateME = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "this route is not for password updates. Please use /updateMyPassword",
+        400
+      )
+    );
+  }
+
+  const filteredBody = filterObj(req.body, "name", "email");
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    reunValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    user: updatedUser,
   });
 });
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -18,7 +18,11 @@ router.route("/forgotPassword").post(authController.forgotPassword);
 router.route("/resetPassword/:token").patch(authController.resetPassword);
 
 router
+  .route("/updateME")
+  .patch(authController.protect, userController.updateME);
+
+router
   .route("/updatePassword")
   .patch(authController.protect, authController.updatePassword);
-  
-  module.exports = router;
+
+module.exports = router;
