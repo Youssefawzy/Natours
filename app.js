f95c8508a8bef9fc85f4f0197ec02333460a7201
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-10 18:24:39 +0300

    feat(security): some security middlewares

--- app.js
+++ app.js
@@ -4,11 +4,42 @@ const userRouter = require("./routes/userRoute");
 const morgan = require("morgan");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController.js");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(express.json({ limit: "10kb" }));
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/v1/tours", tourRouter);
