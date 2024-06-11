7a3073a64d4cbd993a9d9b4d0b2e4313edb1da64
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-11 14:28:03 +0300

    deleted the node_moules form the repo

--- modules/tourModel.js
+++ modules/tourModel.js
@@ -8,8 +8,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, "A tour must have a name"],
       unique: true,
       trim: true,
-      maxlength: [40, "A tour name must have less or equal the 40 characters"],
-      minlength: [10, "A tour name must have more or equal the 10 characters"],
+      maxlength: [40, "A tour name must have less or equal then 40 characters"],
+      minlength: [10, "A tour name must have more or equal then 10 characters"],
+      // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -29,10 +30,11 @@ const tourSchema = new mongoose.Schema(
       },
     },
     ratingsAverage: {
-      type: String,
+      type: Number,
       default: 4.5,
-      min: [1, "the rating must be above 1.0"],
-      max: [1, "the rating must be below 5.0"],
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
+      set: (val) => Math.round(val * 10) / 10, // 4.666666, 46.6666, 47, 4.7
     },
     ratingsQuantity: {
       type: Number,
@@ -40,14 +42,17 @@ const tourSchema = new mongoose.Schema(
     },
     price: {
       type: Number,
-      requaired: [true, "A tour must have a price"],
+      required: [true, "A tour must have a price"],
     },
     priceDiscount: {
       type: Number,
-      validator: function (val) {
-        return val < this.price;
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: "Discount price ({VALUE}) should be below regular price",
       },
-      message: "Discount price ({VALUE}) should be below regular price",
     },
     summary: {
       type: String,
@@ -66,13 +71,43 @@ const tourSchema = new mongoose.Schema(
     createdAt: {
       type: Date,
       default: Date.now(),
-      select: true,
+      select: false,
     },
     startDates: [Date],
     secretTour: {
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: "Point",
+          enum: ["Point"],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
