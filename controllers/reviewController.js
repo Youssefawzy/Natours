818b101952b87d0e7bd7ab82fac0f7543b5e4ea8
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-12 14:11:21 +0300

    feat(Reviews): now we can get all reviews for a tour

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,7 +2,10 @@ const Review = require("./../modules/reviewModel");
 const catchAsync = require("./../utils/catchAsync");
 
 exports.getAllReviews = catchAsync(async (req, res) => {
-  const reviews = await Review.find();
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: "success",
@@ -14,7 +17,6 @@ exports.getAllReviews = catchAsync(async (req, res) => {
 exports.createReview = catchAsync(async (req, res) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
-
 
   const review = await Review.create(req.body);
 
