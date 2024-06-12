52694921eaa07c6bce339e3346ab6d9e60e39ec3
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-12 17:32:58 +0300

    refactor(user router): refactor the file

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,22 +3,6 @@ const router = express.Router();
 const authController = require("./../controllers/authController");
 const userController = require("./../controllers/userController");
 
-router
-  .route("/me")
-  .get(authController.protect, userController.getMe, userController.getUser);
-
-router
-  .route("/deleteMe")
-  .delete(authController.protect, userController.deleteMe);
-
-router.route("/").get(userController.getAllUsers);
-
-router
-  .route("/:id")
-  .get(userController.getUser)
-  .delete(userController.deleteUser)
-  .patch(userController.updateUser);
-
 router.route("/signup").post(authController.signUp);
 
 router.route("/login").post(authController.login);
@@ -27,12 +11,26 @@ router.route("/forgotPassword").post(authController.forgotPassword);
 
 router.route("/resetPassword/:token").patch(authController.resetPassword);
 
-router
-  .route("/updateME")
-  .patch(authController.protect, userController.updateME);
+router.use(authController.protect);
+
+router.route("/updatePassword").patch(authController.updatePassword);
 
 router
-  .route("/updatePassword")
-  .patch(authController.protect, authController.updatePassword);
+  .route("/me")
+  .get(authController.protect, userController.getMe, userController.getUser);
+
+router.route("/updateME").patch(userController.updateME);
+
+router.route("/deleteMe").delete(userController.deleteMe);
+
+router.use(authController.restricTo("admin"));
+
+router.route("/").get(userController.getAllUsers);
+
+router
+  .route("/:id")
+  .get(userController.getUser)
+  .delete(userController.deleteUser)
+  .patch(userController.updateUser);
 
 module.exports = router;
