88bfc871807b098cdc5092c272817a29fe94c65d
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-14 18:33:05 +0300

    feat(tours): create get distances logiv

--- modules/tourModel.js
+++ modules/tourModel.js
@@ -118,6 +118,7 @@ const tourSchema = new mongoose.Schema(
 
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
+
 tourSchema.index({ startLocation: "2dsphere" });
 
 tourSchema.virtual("durationWeeks").get(function () {
@@ -150,12 +151,12 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.pre("aggregate", function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+// tourSchema.pre("aggregate", function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 
-  console.log(this.pipeline());
-  next();
-});
+//   console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model("Tour", tourSchema);
 
