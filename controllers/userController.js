673164ca23a386399ed628870551cf178a7b49e5
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-17 18:00:04 +0300

    feat(users): now the user can upload and update his photo

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,39 @@ const AppError = require("../utils/appError");
 const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const factory = require("./../controllers/handlerFactoru");
+const multer = require("multer");
+const sharp = require("sharp");
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new AppError("Not an image! Please upload only images.", 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single("photo");
+
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+});
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -12,6 +45,8 @@ const filterObj = (obj, ...allowedFields) => {
 };
 
 exports.updateME = catchAsync(async (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
   if (req.body.password || req.body.passwordConfirm) {
     return next(
       new AppError(
@@ -19,10 +54,10 @@ exports.updateME = catchAsync(async (req, res, next) => {
         400
       )
     );
-  }
+  } 
 
   const filteredBody = filterObj(req.body, "name", "email");
-
+  if (req.file) filteredBody.photo = req.file.filename; 
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     reunValidators: true,
