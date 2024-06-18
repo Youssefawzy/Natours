ea23e8192b7c931e57633870e7b143646b01d076
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-18 12:34:30 +0300

    feat(views router): added the getmytours and updateUserData routers

--- routes/viewRoute.js
+++ routes/viewRoute.js
@@ -3,10 +3,19 @@ const viewsController = require("../controllers/viewsController");
 const authController = require("./../controllers/authController");
 
 const router = express.Router();
+router.use(viewsController.alerts);
 
-// router.use(authController.isLoggedIn);
+router.get("/", authController.isLoggedIn, viewsController.getOverview);
 
-router.get("/", viewsController.getOverview);
-router.get("/tour/:slug", viewsController.getTour);
+router.get("/tour/:slug", authController.isLoggedIn, viewsController.getTour);
 router.get("/login", authController.isLoggedIn, viewsController.getLoginForm);
+
+router.get("/my-tours", authController.protect, viewsController.getMyTours);
+
+router.post(
+  "/submit-user-data",
+  authController.protect,
+  viewsController.updateUserData
+);
+
 module.exports = router;
