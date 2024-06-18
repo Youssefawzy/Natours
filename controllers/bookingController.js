05c0b095acce642c5b34c3984d9e8208f7077277
Youssefawzy <116081592+Youssefawzy@users.noreply.github.com>
2024-06-18 12:40:44 +0300

    feat(booking): add the all the factory functions to booking

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -37,3 +37,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     session,
   });
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
